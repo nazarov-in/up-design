6a34a339623ca8b6003d024078da9ad2276d6e60
Vitali <seonazarov@yandex.ru>
2020-05-04 14:09:58 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -3,5 +3,54 @@ $(function () {
     $(document).ready(function() {
         $('select').niceSelect();
       });
-      
+
+    //Модальное окно
+    $('#base').click(function(e){
+        e.preventDefault();
+        $('.plan__modal_base').css('display', 'block'); 
+    });
+
+    $('.close_mod').click(function () {
+        $('.plan__modal_base').css('display', 'none');
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $(".plan__modal_base");
+        if (container.has(e.target).length === 0){   
+            container.hide();
+        }
+    });
+
+    $('#optimal').click(function(e){
+        e.preventDefault();
+        $('.plan__modal_optimal').css('display', 'block');
+    });
+
+    $('.close_mod').click(function () {
+        $('.plan__modal_optimal').css('display', 'none');
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $(".plan__modal_optimal");
+        if (container.has(e.target).length === 0){    
+            container.hide();
+        }
+    });
+
+    $('#inclusive').click(function(e){
+        e.preventDefault();
+        $('.plan__modal_inclusive').css('display', 'block');  
+    });
+
+    $('.close_mod').click(function () {
+        $('.plan__modal_inclusive').css('display', 'none');    
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $(".plan__modal_inclusive");
+        if (container.has(e.target).length === 0){ 
+            container.hide();
+        }
+    });
+
 });
